4e6d1f458c2e02fbad358559b8127d2a6956688d
Elecomexp <lander.combarro@outlook.com>
2024-06-20 14:05:41 +0200

    Lander pseudocódigo

---
+++ Battleship_Lander/variables.py
@@ -0,0 +1,11 @@
+# Dimension del tablero
+DIMENSION_TABLERO = 10
+
+# Definición de los barcos:
+# str(nombre_barco) : tuple(eslora, numero_de_barcos_en_el_juego)
+BARCOS = {
+    'Portaaviones' : (4, 1),
+    'Acorazado'    : (3, 2),
+    'Submarino'    : (2, 3),
+    'Destructor'   : (1, 4)
+    }
